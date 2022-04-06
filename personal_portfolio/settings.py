4f2a0e3649b127b34bcc4e91e765eb4407d68963
Salavat <karimov.slvt@gmail.com>
2022-04-06 14:44:25 +0000

    Added files

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-po_liopl0c#e0b*x8!t!&oe^ycug&sx0s(lvobb$l*1-4b#!_2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['salavat.pythonanywhere.com']
 
 
 # Application definition
